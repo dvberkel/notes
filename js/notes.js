8eaab0d14d1fc68a573d2dabee7ffcb9b3377121
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-04-19 09:18:55 +0200

    NoteModel introduced
    A NoteModel is introduced which is responsible for holding all of the
    Note state.

--- js/notes.js
+++ js/notes.js
@@ -1,17 +1,25 @@
 (function($, _, Backbone, undefined){
+    var NoteModel = Backbone.Model.extend({
+	defaults : {content: "Click to change"}
+    });
+
     var NoteView = Backbone.View.extend({
 	initialize : function(){
 	    _.bindAll(this, 'render');
+	    
+	    this.model.bind("change", function(){
+		this.render();
+	    },this);
 	    
 	    this.render();
 	},
 	
 	render: function(){
-	    $(this.el).html('Note');
+	    $(this.el).html(this.model.get('content'));
 	}
     });
 
     $(function(){
-	new NoteView({el: $("#note")});
+	new NoteView({el: $("#note"), model: new NoteModel()});
     });      
 })(jQuery, _, Backbone);
